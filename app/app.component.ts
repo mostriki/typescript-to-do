253d1cc4a7afc8e9d9e28647723d02f27b770690
Riki Montgomery <mostriki820@gmail.com>
2017-10-24 16:32:57 -0700

    added double event binding functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor='let currentTask of tasks'>{{currentTask.description}} <button class="btn btn-primary" (click)="editTask()">Edit!</button></li>
-    </ul>
-  </div>
-  `
+      <div class="container">
+       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+       <h3>{{currentFocus}}</h3>
+       <ul>
+        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+      </ul>
+      <hr>
+      <div>
+       <h3>{{selectedTask.description}}</h3>
+       <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+       <label>Enter Task Priority (1-3):</label>
+       <br>
+       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      </div>
+    </div>
+    `
 })
 
 export class AppComponent {
@@ -21,12 +34,13 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming [possible Javascript group projects', 2),
-    new Task('Add README file to last few Angular repos on Github', 2)
-  ];
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)  ];
+  selectedTask: Task = this.tasks[0];
 
-  editTask() {
-    alert("You just registered to edit a Task!");
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task) {
@@ -36,20 +50,20 @@ export class AppComponent {
       alert("This task is not done. Better get to work!");
     }
   }
-  priorityColor(currentTask) {
-    if (currentTask.priority === 3) {
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
       return "bg-danger";
     } else if (currentTask.priority === 2) {
-      return "bg-warning";
+      return  "bg-warning";
     } else {
-      retrun "bg-info";
+      return "bg-info";
     }
   }
-};
+
+}
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string, public priority: number) {
-
-  }
+  constructor(public description: string, public priority: number) { }
 }
