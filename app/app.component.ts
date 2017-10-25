268ec75517ae54a043531d34bc8cca86e0a426d8
Riki Montgomery <mostriki820@gmail.com>
2017-10-24 17:36:41 -0700

    add conditional directives to the to-do list app

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
         <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
       </ul>
       <hr>
-      <div>
+      <div *ngIf="selectedTask">
        <h3>{{selectedTask.description}}</h3>
        <p>Task Complete? {{selectedTask.done}}</p>
       <h3>Edit Task</h3>
@@ -21,6 +21,7 @@ import { Component } from '@angular/core';
        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+       <button (click)="finishedEditing()">Done</button>
       </div>
     </div>
     `
@@ -36,11 +37,15 @@ export class AppComponent {
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)  ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 
   isDone(clickedTask: Task) {
@@ -60,7 +65,6 @@ export class AppComponent {
       return "bg-info";
     }
   }
-
 }
 
 export class Task {
