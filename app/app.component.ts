160f17e598a83b01eb9ed62f6b4d9f264b9121bb
Dan Danilyuk and Riki Montgomery <riki@>
2017-10-25 10:55:51 -0700

    added componenets, create new task, and linking

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,18 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   template: `
-      <div class="container">
-       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-       <h3>{{currentFocus}}</h3>
-       <ul>
-        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-      </ul>
+    <div class="container">
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
       <hr>
-      <div *ngIf="selectedTask">
-       <h3>{{selectedTask.description}}</h3>
-       <p>Task Complete? {{selectedTask.done}}</p>
-      <h3>Edit Task</h3>
-      <label>Enter Task Description:</label>
-      <input [(ngModel)]="selectedTask.description">
-       <label>Enter Task Priority (1-3):</label>
-       <br>
-       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-       <button (click)="finishedEditing()">Done</button>
-      </div>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+      <new-task (newTaskSender)="addTask($event)"></new-task>
     </div>
-    `
+  `
 })
 
 export class AppComponent {
@@ -33,12 +21,13 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)  ];
   selectedTask = null;
 
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -48,26 +37,8 @@ export class AppComponent {
     this.selectedTask = null;
   }
 
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not done. Better get to work!");
-    }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority: number) { }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule }  from '@angular/forms';
+import { TaskListComponent }  from './task-list.component';
+import { EditTaskComponent }  from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
 
 @NgModule({
   imports: [ BrowserModule,
-                  FormsModule ],
-  declarations: [ AppComponent ],
+            FormsModule ],
+  declarations: [ AppComponent,
+                  TaskListComponent,
+                  EditTaskComponent,
+                  NewTaskComponent],
   bootstrap:    [ AppComponent ]
 })
 
